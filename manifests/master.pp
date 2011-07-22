82539d216fe4335b9865011b1022558c24a9c938
mh <mh@immerda.ch>
2011-07-22 13:22:51 +0200

    correct interpolation

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,6 +1,6 @@
 class hiera::master {
   require rubygems::hiera_puppet
-  file{"#{settings::confdir}/hiera.yaml":
+  file{"${settings::confdir}/hiera.yaml":
     source => 'puppet:///modules/site-hiera/master/hiera.yaml',
     owner => root, group => puppet, mode => 0640;
   }
