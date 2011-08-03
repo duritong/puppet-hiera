05636792f5612f5abfa78b8acafa44b01ad2150f
Marcel Haerry <haerry@puzzle.ch>
2011-08-03 16:49:47 +0200

    fix a nil vs. false issue

--- lib/puppet/parser/functions/hiera.rb
+++ lib/puppet/parser/functions/hiera.rb
@@ -39,7 +39,7 @@ module Puppet::Parser::Functions
 
         answer = hiera.lookup(key, default, hiera_scope, override, :priority)
 
-        raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") unless answer
+        raise(Puppet::ParseError, "Could not find data item #{key} in any Hiera data file and no default supplied") if answer.nil?
 
         return answer
     end
