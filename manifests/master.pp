d9fab30f54f8de05a946216e90c5c4ec99d29b2a
mh <mh@immerda.ch>
2012-04-15 16:06:03 +0200

    dashes in class/module names will be unsupported

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,7 +1,7 @@
 class hiera::master {
   require rubygems::hiera_puppet
   file{"${settings::confdir}/hiera.yaml":
-    source => 'puppet:///modules/site-hiera/master/hiera.yaml',
+    source => 'puppet:///modules/site_hiera/master/hiera.yaml',
     owner => root, group => puppet, mode => 0640;
   }
 }
